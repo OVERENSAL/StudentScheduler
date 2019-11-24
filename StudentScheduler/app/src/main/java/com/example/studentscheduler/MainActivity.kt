7650759f9b1f9368c9603aa51ef85a2191aba959
OVERENSAL <mle8@yandex.ru>
2019-11-24 20:22:00 +0300

    add plus/minusDate

--- StudentScheduler/app/src/main/java/com/example/studentscheduler/MainActivity.kt
+++ StudentScheduler/app/src/main/java/com/example/studentscheduler/MainActivity.kt
@@ -1,41 +1,44 @@
 package com.example.studentscheduler
 
+import android.app.Application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.jakewharton.threetenabp.AndroidThreeTen
 import kotlinx.android.synthetic.main.activity_main.*
 import org.threeten.bp.ZonedDateTime
 import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
+import org.threeten.bp.format.DateTimeFormatter
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    var globalDate = ZonedDateTime.now()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val day = currentDate2.plusDays(1)
-
         AndroidThreeTen.init(this)
-
         val currentDate2 = ZonedDateTime.now()
-        date.text = currentDate2.toString()
+        date.text = currentDate2.format(DateTimeFormatter.ofPattern("dd.M.yyyy"))
     }
 
-  fun prevDate(view: View) {
-        val prevDate = SimpleDateFormat("dd.M.yyyy").format(Date())
-
-        date.text = prevDate
+    fun prevDate(view: View) {
+        val currentDate = globalDate
+        val prevDate = currentDate.minusDays(1)
+        globalDate = prevDate
+        date.text = prevDate.format(DateTimeFormatter.ofPattern("dd.M.yyyy"))
     }
 
     fun nextDate(view: View) {
-        val nextDate = SimpleDateFormat("dd.M.yyyy").format(Date())
-
-        date.text = nextDate
+        val currentDate = globalDate
+        val nextDate = currentDate.plusDays(1)
+        globalDate = nextDate
+        date.text = nextDate.format(DateTimeFormatter.ofPattern("dd.M.yyyy"))
     }
 
     fun addTasksActivity(view: View) {
